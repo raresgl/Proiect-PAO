2ea40fa399f65f4841eb6ca5cce94d322297379d
Rares Mateiu <40142811+raresgl@users.noreply.github.com>
2021-04-28 00:15:08 +0300

    Add files via upload

---
+++ Proiect-PAO-master/src/medical_appointment/AuditService.java
@@ -0,0 +1,27 @@
+package medical_appointment;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.sql.Timestamp;
+import java.util.Date;
+
+abstract public class AuditService {
+
+    private static File auditFile = new File("results/AuditFile.csv");
+
+    public static void logAuditFile(String actionName)
+    {
+        try {
+            FileWriter fw = new FileWriter(auditFile, true);
+            Date d = new Date();
+            long t = d.getTime();
+            Timestamp ts = new Timestamp(t);
+            fw.write(actionName + "," +  ts.toString());
+            fw.write("\n");
+            fw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ Proiect-PAO-master/src/medical_appointment/Recenzie.java
@@ -0,0 +1,48 @@
+package medical_appointment;
+
+public class Recenzie {
+    private Integer nota;
+    private Pacient pacientReviewer;
+    private Medic medic;
+    private ServiciuMedical serviciuMedical;
+
+
+    public Recenzie(Integer nota, Pacient pacientReviewer, Medic medic, ServiciuMedical serviciuMedical) {
+        this.nota = nota;
+        this.pacientReviewer = pacientReviewer;
+        this.medic = medic;
+        this.serviciuMedical = serviciuMedical;
+    }
+
+    public Integer getNota() {
+        return nota;
+    }
+
+    public void setNota(Integer nota) {
+        this.nota = nota;
+    }
+
+    public Pacient getPacientReviewer() {
+        return pacientReviewer;
+    }
+
+    public void setPacientReviewer(Pacient pacientReviewer) {
+        this.pacientReviewer = pacientReviewer;
+    }
+
+    public Medic getMedic() {
+        return medic;
+    }
+
+    public void setMedic(Medic medic) {
+        this.medic = medic;
+    }
+
+    public ServiciuMedical getServiciuMedical() {
+        return serviciuMedical;
+    }
+
+    public void setServiciuMedical(ServiciuMedical serviciuMedical) {
+        this.serviciuMedical = serviciuMedical;
+    }
+}
